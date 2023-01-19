3b9b22264c3a001bbf7f4570626774a14d160637
Shintaro K <fj68forg@gmail.com>
2023-01-19 20:28:50 +0000

    add some builtin conversion function

--- interp/builtins.go
+++ interp/builtins.go
@@ -2,6 +2,8 @@ package interp
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 	"strings"
 )
 
@@ -9,6 +11,10 @@ var DefaultBuiltins = map[string]Value{
 	"print":    VBuiltinFun(builtinPrint),
 	"get_type": VBuiltinFun(builtinGetType),
 	"bool":     VBuiltinFun(builtinBool),
+	"number":   VBuiltinFun(builtinNumber),
+	"ceil":     VBuiltinFun(builtinCeil),
+	"floor":    VBuiltinFun(builtinFloor),
+	"string":   VBuiltinFun(builtinString),
 }
 
 func builtinPrint(s *State, args []Value) (Value, error) {
@@ -42,6 +48,73 @@ func builtinBool(s *State, args []Value) (Value, error) {
 		return VBool(v != nil), nil
 	case VBuiltinFun:
 		return VBool(v != nil), nil
+	}
+	return nil, fmt.Errorf("unknown value type: %s", args[0].Type().String())
+}
+
+func builtinNumber(s *State, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("too many / less arguments for number()")
+	}
+	switch v := args[0].(type) {
+	case VBool:
+		if v {
+			return VNumber(1), nil
+		}
+		return VNumber(0), nil
+	case VNumber:
+		return v, nil
+	case VString:
+		n, err := strconv.ParseFloat(string(v), 64)
+		if err != nil {
+			return nil, err
+		}
+		return VNumber(n), nil
+	case *VUserFun:
+		return nil, fmt.Errorf("unable to convert function to number")
+	case VBuiltinFun:
+		return nil, fmt.Errorf("unable to convert function to number")
+	}
+	return nil, fmt.Errorf("unknown value type: %s", args[0].Type().String())
+}
+
+func builtinCeil(s *State, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("too many / less arguments for number()")
+	}
+	v, ok := args[0].(VNumber)
+	if !ok {
+		return nil, fmt.Errorf("argument for ceil() is expected number, but got %s", v.Type())
+	}
+	return VNumber(math.Ceil(float64(v))), nil
+}
+
+func builtinFloor(s *State, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("too many / less arguments for number()")
+	}
+	v, ok := args[0].(VNumber)
+	if !ok {
+		return nil, fmt.Errorf("argument for floor() is expected number, but got %s", v.Type())
+	}
+	return VNumber(math.Floor(float64(v))), nil
+}
+
+func builtinString(s *State, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("too many / less arguments for number()")
+	}
+	switch v := args[0].(type) {
+	case VBool:
+		return VString(fmt.Sprintf("%t", v)), nil
+	case VNumber:
+		return VString(fmt.Sprintf("%g", v)), nil
+	case VString:
+		return v, nil
+	case *VUserFun:
+		return VString(v.Type().String()), nil
+	case VBuiltinFun:
+		return VString(v.Type().String()), nil
 	}
 	return nil, fmt.Errorf("unknown value type: %s", args[0].Type().String())
 }
