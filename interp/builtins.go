e6b413e79db50fbd600fa6b180cd62223d120fbd
Shintaro K <fj68forg@gmail.com>
2023-01-19 20:14:21 +0000

    add builtin bool()

--- interp/builtins.go
+++ interp/builtins.go
@@ -8,6 +8,7 @@ import (
 var DefaultBuiltins = map[string]Value{
 	"print":    VBuiltinFun(builtinPrint),
 	"get_type": VBuiltinFun(builtinGetType),
+	"bool":     VBuiltinFun(builtinBool),
 }
 
 func builtinPrint(s *State, args []Value) (Value, error) {
@@ -21,7 +22,26 @@ func builtinPrint(s *State, args []Value) (Value, error) {
 
 func builtinGetType(s *State, args []Value) (Value, error) {
 	if len(args) != 1 {
-		return nil, fmt.Errorf("too many / less arguments for typeof()")
+		return nil, fmt.Errorf("too many / less arguments for get_type()")
 	}
 	return VString(args[0].Type().String()), nil
+}
+
+func builtinBool(s *State, args []Value) (Value, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("too many / less arguments for bool()")
+	}
+	switch v := args[0].(type) {
+	case VBool:
+		return v, nil
+	case VNumber:
+		return VBool(v != 0), nil
+	case VString:
+		return VBool(v == "true"), nil
+	case *VUserFun:
+		return VBool(v != nil), nil
+	case VBuiltinFun:
+		return VBool(v != nil), nil
+	}
+	return nil, fmt.Errorf("unknown value type: %s", args[0].Type().String())
 }
