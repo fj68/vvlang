65287b42572163f6407c2b7d3b06a1345192a45e
Shintaro K <fj68forg@gmail.com>
2026-02-24 05:53:07 +0000

    Add `vv run` command

---
+++ cmd/run.go
@@ -0,0 +1,93 @@
+package cmd
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+
+	"github.com/fj68/vvlang/interp"
+	"github.com/fj68/vvlang/interp/builtins"
+	"github.com/fj68/vvlang/mod"
+)
+
+func Run(stdlib fs.FS) {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: vv [path]")
+		return
+	}
+	if len(os.Args) > 3 {
+		fmt.Println("usage: vv run [path]")
+		return
+	}
+
+	var path string
+	if os.Args[1] == "run" {
+		path = os.Args[2]
+	} else {
+		path = os.Args[1]
+	}
+
+	if err := run(path, stdlib); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func run(path string, stdlib fs.FS) error {
+	text, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+	s := interp.NewState(path)
+
+	var moduleBuiltins = map[string]map[string]interp.Value{
+		"std/sys.vv": {
+			"help": interp.VBuiltinFun(builtins.Help),
+		},
+		"std/console.vv": {
+			"print": interp.VBuiltinFun(builtins.Print),
+		},
+		"std/math.vv": {
+			"floor": interp.VBuiltinFun(builtins.Floor),
+			"ceil":  interp.VBuiltinFun(builtins.Ceil),
+		},
+		"std/string.vv": {
+			"length": interp.VBuiltinFun(builtins.StringLength),
+		},
+		"std/list.vv": {
+			"length": interp.VBuiltinFun(builtins.ListLength),
+			"push":   interp.VBuiltinFun(builtins.Push),
+		},
+		"std/float.vv": {
+			"to_string": interp.VBuiltinFun(builtins.FloatToString),
+		},
+		"std/int.vv": {
+			"to_string": interp.VBuiltinFun(builtins.IntToString),
+		},
+		"std/bool.vv": {
+			"to_string": interp.VBuiltinFun(builtins.BoolToString),
+		},
+	}
+
+	s.NewState = func(sourcePath string) *interp.State {
+		ns := interp.NewState(sourcePath)
+
+		// Register module-specific built-ins
+		for stdPath, funcs := range moduleBuiltins {
+			if sourcePath == mod.GetPackagePath(stdPath) {
+				ns.RegisterNatives(funcs)
+				break
+			}
+		}
+
+		ns.NewState = s.NewState
+		return ns
+	}
+	if err := s.EnsureSystemLibrary("std", stdlib); err != nil {
+		return err
+	}
+	if err := s.Eval([]rune(string(text))); err != nil {
+		return err
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -5,14 +5,11 @@ import (
 	"os"
 
 	"github.com/fj68/vvlang/cmd"
-	"github.com/fj68/vvlang/interp"
-	"github.com/fj68/vvlang/interp/builtins"
-	"github.com/fj68/vvlang/mod"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: vv [run path | get path | clean path]")
+		fmt.Println("usage: vv [run|get|clean] path")
 		return
 	}
 	switch os.Args[1] {
@@ -21,67 +18,7 @@ func main() {
 	case "clean":
 		cmd.Clean()
 	default:
-		run(os.Args[1])
+		cmd.Run(stdlib)
 	}
 }
 
-func run(path string) {
-	text, err := os.ReadFile(path)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	s := interp.NewState(path)
-
-	var moduleBuiltins = map[string]map[string]interp.Value{
-		"std/sys.vv": {
-			"help": interp.VBuiltinFun(builtins.Help),
-		},
-		"std/console.vv": {
-			"print": interp.VBuiltinFun(builtins.Print),
-		},
-		"std/math.vv": {
-			"floor": interp.VBuiltinFun(builtins.Floor),
-			"ceil":  interp.VBuiltinFun(builtins.Ceil),
-		},
-		"std/string.vv": {
-			"length": interp.VBuiltinFun(builtins.StringLength),
-		},
-		"std/list.vv": {
-			"length": interp.VBuiltinFun(builtins.ListLength),
-			"push":   interp.VBuiltinFun(builtins.Push),
-		},
-		"std/float.vv": {
-			"to_string": interp.VBuiltinFun(builtins.FloatToString),
-		},
-		"std/int.vv": {
-			"to_string": interp.VBuiltinFun(builtins.IntToString),
-		},
-		"std/bool.vv": {
-			"to_string": interp.VBuiltinFun(builtins.BoolToString),
-		},
-	}
-
-	s.NewState = func(sourcePath string) *interp.State {
-		ns := interp.NewState(sourcePath)
-
-		// Register module-specific built-ins
-		for stdPath, funcs := range moduleBuiltins {
-			if sourcePath == mod.GetPackagePath(stdPath) {
-				ns.RegisterNatives(funcs)
-				break
-			}
-		}
-
-		ns.NewState = s.NewState
-		return ns
-	}
-	if err := s.EnsureSystemLibrary("std", stdlib); err != nil {
-		fmt.Println("EnsureSystemLibrary", err)
-		return
-	}
-	if err := s.Eval([]rune(string(text))); err != nil {
-		fmt.Println(err)
-		return
-	}
-}
