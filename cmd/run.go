7d60927cc4fc73ea03ad1628b5d07975c697e386
Shintaro K <fj68forg@gmail.com>
2026-02-24 07:34:22 +0000

    Move definition of builtin modules to std.go

--- cmd/run.go
+++ cmd/run.go
@@ -5,7 +5,6 @@ import (
 	"os"
 
 	"github.com/fj68/vvlang/interp"
-	"github.com/fj68/vvlang/interp/builtins"
 	"github.com/fj68/vvlang/lib"
 )
 
@@ -38,36 +37,7 @@ func run(path string) error {
 	}
 	s := interp.NewState(path)
 
-	var moduleBuiltins = map[string]map[string]interp.Value{
-		"std/sys.vv": {
-			"help": interp.VBuiltinFun(builtins.Help),
-		},
-		"std/console.vv": {
-			"print": interp.VBuiltinFun(builtins.Print),
-		},
-		"std/math.vv": {
-			"floor": interp.VBuiltinFun(builtins.Floor),
-			"ceil":  interp.VBuiltinFun(builtins.Ceil),
-		},
-		"std/string.vv": {
-			"length": interp.VBuiltinFun(builtins.StringLength),
-		},
-		"std/list.vv": {
-			"length": interp.VBuiltinFun(builtins.ListLength),
-			"push":   interp.VBuiltinFun(builtins.Push),
-		},
-		"std/float.vv": {
-			"to_string": interp.VBuiltinFun(builtins.FloatToString),
-		},
-		"std/int.vv": {
-			"to_string": interp.VBuiltinFun(builtins.IntToString),
-		},
-		"std/bool.vv": {
-			"to_string": interp.VBuiltinFun(builtins.BoolToString),
-		},
-	}
-
-	s.RegisterBuiltinModules(moduleBuiltins)
+	s.RegisterBuiltinModules(lib.BuiltinModules)
 
 	if err := s.EnsureSystemLibrary("std", lib.Std); err != nil {
 		return err
--- lib/std.go
+++ lib/std.go
@@ -2,7 +2,40 @@ package lib
 
 import (
 	"embed"
+
+	"github.com/fj68/vvlang/interp"
+	"github.com/fj68/vvlang/interp/builtins"
 )
 
 //go:embed std
 var Std embed.FS
+
+var BuiltinModules = map[string]map[string]interp.Value{
+	"std/sys.vv": {
+		"help": interp.VBuiltinFun(builtins.Help),
+	},
+	"std/console.vv": {
+		"print": interp.VBuiltinFun(builtins.Print),
+	},
+	"std/math.vv": {
+		"floor": interp.VBuiltinFun(builtins.Floor),
+		"ceil":  interp.VBuiltinFun(builtins.Ceil),
+	},
+	"std/string.vv": {
+		"length": interp.VBuiltinFun(builtins.StringLength),
+	},
+	"std/list.vv": {
+		"length": interp.VBuiltinFun(builtins.ListLength),
+		"push":   interp.VBuiltinFun(builtins.Push),
+	},
+	"std/float.vv": {
+		"to_string": interp.VBuiltinFun(builtins.FloatToString),
+	},
+	"std/int.vv": {
+		"to_string": interp.VBuiltinFun(builtins.IntToString),
+	},
+	"std/bool.vv": {
+		"to_string": interp.VBuiltinFun(builtins.BoolToString),
+	},
+}
+
